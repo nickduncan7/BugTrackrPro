b33760ae9898df4830c8f25ccbd8aa0a7fe45cf7
Nick Duncan <sorrowspear@gmail.com>
2017-01-30 15:20:15 -0700

    Can add bugs now.
    The system for this is horrible... I apologize.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <div id="header">
       <img src="./assets/logo.svg">
     </div>
-    <bug></bug>
+    <bug v-for="bug in bugs"></bug>
+    <div id="add-bug">
+      <button @click="addBug">+ Add New Bug</button>
+    </div>
   </div>
 </template>
 
@@ -12,7 +15,21 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js Crapp'
+      bugs: [],
+      nextBugId: 0
+    }
+  },
+
+  mounted: function () {
+    if (!this.bugs) { console.log("Error initializing array of bugs... you have one (real) bug now."); return; }
+
+    for(var i = 0; i < Math.floor((Math.random() *(7 - 3) + 3)); i++)
+      this.addBug();
+  },
+
+  methods: {
+    addBug: function () {
+      this.bugs.push(this.nextBugId++);
     }
   }
 }
@@ -23,10 +40,38 @@ html, body, #app {
   margin: 0;
   padding: 0;
   background: #C5C3C6;
+  font-family: 'Source Sans Pro', sans-serif;
+  color: #46494c;
 }
 
 #header {
   background: #DCDCDD;
   padding: 24px 12em;
 }
+
+#add-bug {
+  width: 512px;
+  margin: 24px auto;
+  text-align: right;
+}
+
+#add-bug button {
+  padding: 4px 16px;
+  background: #dcdcdd;
+  border: 0;
+  border-radius: 5px;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+}
+
+#add-bug button {
+    outline: none;
+}
+
+#add-bug button:hover {
+  background: #fff;
+  cursor: pointer;
+  
+}
+
 </style>
--- src/Bug.vue
+++ src/Bug.vue
@@ -1,26 +1,46 @@
 <template>
     <div class="bug">
-        <input class="progress-range" :style="{ background: backgroundString }" type="range" :value="progress" min="0" max="100" @input="updateSlider">
+        <div class="title" contenteditable="true" placeholder="Bug name...">
+            {{ name }}
+        </div>
+        <div class="progress-area">
+            <input class="progress-range" :style="{ background: backgroundString }" type="range" :value="progress" min="1" max="100"
+                @input="updateSlider">
+            <div class="progress-number">{{ progress | percent }}</div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            name: "Bug",
-            progress: 50,
-            backgroundString: "-webkit-linear-gradient(left, #1985A1 0%, #1985A1 50%, #C5C3C6 50%)"
-        }
-    },
+    export default {
+        data() {
+            return {
+                name: "Bug #" + Math.floor((Math.random() *(10000 - 1000) + 1000)),
+                progress: Math.floor((Math.random() *(80 - 30) + 30)),
+                backgroundString: "-webkit-linear-gradient(left, #1985A1 0%, #1985A1 " + this.progress + "%, #C5C3C6 " + this.progress + "%)"
+            }
+        },
 
-    methods: {
-        updateSlider: function (input) {
-            this.progress = input.target.valueAsNumber;
-            this.backgroundString = "-webkit-linear-gradient(left, #1985A1 0%, #1985A1 "+ this.progress + "%, #C5C3C6 "+ this.progress + "%)";
+        filters: {
+            percent: function (value) {
+                if (!value) return '';
+
+                value = value.toString();
+                return value + '%';
+            }
+        },
+
+        methods: {
+            updateSlider: function (input) {
+                this.progress = input.target.valueAsNumber;
+                this.backgroundString = "-webkit-linear-gradient(left, #1985A1 0%, #1985A1 " + this.progress + "%, #C5C3C6 " + this.progress + "%)";
+            }
+        },
+
+        mounted: function () {
+            this.backgroundString = "-webkit-linear-gradient(left, #1985A1 0%, #1985A1 " + this.progress + "%, #C5C3C6 " + this.progress + "%)";
         }
     }
-}
 </script>
 
 <style>
@@ -32,13 +52,36 @@ export default {
     padding: 16px;
 }
 
+.bug .title {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+    margin-bottom: 8px;
+}
+
+.progress-area {
+    display: flex;
+}
+
 .progress-range {
     -webkit-appearance: none;
     border-radius: 5px;
     padding: 0;
     margin: 0;
+    margin-top: 7px;
     width: 100%;
     height: 10px;
+}
+
+.progress-number {
+    height: 20px;
+    border-radius: 2px;
+    background: #4c5c68;
+    color: #fff;
+    font-family: 'Montserrat', sans-serif;
+    padding: 2px;
+    margin-left: 8px;
+    width: 55px;
+    text-align: center;
 }
 
 .progress-range::-webkit-slider-thumb {
@@ -57,5 +100,11 @@ export default {
 
 .progress-range:focus {
     outline: none;
+}
+
+[contenteditable=true]:empty:before{
+  content: attr(placeholder);
+  display: block;
+  color: #889;
 }
 </style>
